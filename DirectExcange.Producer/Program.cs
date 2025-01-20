021fe24988c15246e63ae48bc49bb1735864510d
Mona400 <monasallam400@gmail.com>
2025-01-20 19:57:33 +0200

    DirectExcange

---
+++ DirectExcange.Producer/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace DirectExcange.Producer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory=new ConnectionFactory { HostName= "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            const string message = " message is sending....!";
+            var body=Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "amq.direct", routingKey: "key-qu-notification", basicProperties: null, body: body);
+            Console.WriteLine($"send {message}");
+            Console.WriteLine("press enter to exist");
+            Console.ReadKey();
+        }
+    }
+}
