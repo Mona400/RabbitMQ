11c7c33a2d3270dae42e05ce3f2504679d1ffffe
Mona400 <monasallam400@gmail.com>
2025-01-25 20:14:50 +0200

    Header Exchange

---
+++ HeaderExchange.Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace HeaderExchange.Producer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            
+            var properties=channel.CreateBasicProperties();
+            properties.Persistent = false;
+
+            Dictionary<string,object>dictionary = new Dictionary<string,object>();
+            dictionary.Add("name", "info");
+            properties.Headers = dictionary;  
+            const string message = " message is sending....!";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "amq.headers", routingKey: string.Empty, basicProperties: properties, body: body);
+            
+            
+            Console.WriteLine($"send {message}");
+            Console.WriteLine("press enter to exist");
+            Console.ReadKey();
+        }
+    }
+}
